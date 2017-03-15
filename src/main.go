80dfb45f3fc4b1f1a5534ef04d27d52ac4522200
vildursn <vildegjaerum@gmail.com>
2017-03-15 21:51:08 +0100

    Update main.go

--- src/main.go
+++ src/main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	/*
-	"driver"
-	"global"
-	"network"
-	"queue"
-	"fsm"
-	"buttonhandler"*/
-	"fsm"
-	"ordermanager"
-	"queue"
+		"driver"
+		"global"
+		"network"
+		"queue"
+		"fsm"
+		"buttonhandler"*/
 	"driver"
 	"fmt"
+	"fsm"
+	"network"
+	"ordermanager"
+	"queue"
 )
 
 func main() {
@@ -23,29 +24,33 @@ func main() {
 	updated_order_chan := make(chan queue.Order, 10)
 	updated_order_bool_chan := make(chan bool)
 	new_order_bool_chan := make(chan bool)
-	
+
 	go fsm.State_handler(new_order_bool_chan, updated_order_bool_chan, updated_order_chan)
 	go queue.Order_handler(new_order_bool_chan, new_order_chan, updated_order_chan)
 	go ordermanager.Button_handler(new_order_chan)
+
+	// test network
+	go network.Network_handler()
+	go network.Network_setup(new_order_bool_chan)
 
 	fmt.Println("I am ready")
 
 	// Make channels
 	/*
-	new_order_chan := make(chan queue.Order, 10)
-	updated_order_chan := make(chan queue.Order, 10)
-	external_order_list_chan := make(chan [global.NUM_EXTERNAL_ORDERS]queue.Order)
-	internal_order_list_chan := make(chan [global.NUM_INTERNAL_ORDERS]queue.Order)
-	new_order_bool_chan := make(chan bool)
-	is_master_chan := make(chan bool)
-*/
+		new_order_chan := make(chan queue.Order, 10)
+		updated_order_chan := make(chan queue.Order, 10)
+		external_order_list_chan := make(chan [global.NUM_EXTERNAL_ORDERS]queue.Order)
+		internal_order_list_chan := make(chan [global.NUM_INTERNAL_ORDERS]queue.Order)
+		new_order_bool_chan := make(chan bool)
+		is_master_chan := make(chan bool)
+	*/
 	// Run all processes
 	/*
-	go network.Network_handler(is_master_chan)
-	go fsm.State_handler(new_order_bool_chan, updated_order_chan, external_order_list_chan, internal_order_list_chan)
-	go queue.Order_handler(new_order_bool_chan, new_order_chan, updated_order_chan, external_order_list_chan, internal_order_list_chan)
-	go buttonhandler.Button_handler(new_order_chan)
-*/
+		go network.Network_handler(is_master_chan)
+		go fsm.State_handler(new_order_bool_chan, updated_order_chan, external_order_list_chan, internal_order_list_chan)
+		go queue.Order_handler(new_order_bool_chan, new_order_chan, updated_order_chan, external_order_list_chan, internal_order_list_chan)
+		go buttonhandler.Button_handler(new_order_chan)
+	*/
 	// Keep on running
 	for {
 	}
