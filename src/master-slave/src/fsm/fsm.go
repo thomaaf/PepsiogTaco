99f2dc590af6a0e18660d5c3dd7ffd5ec73da562
vildursn <vildegjaerum@gmail.com>
2017-03-16 15:43:26 +0100

    Update fsm.go
    Problem floor 4: thinks it's still an order (3-4 from 1 or 2)
    Problem floor 1: thinks zero order is an order (1-2 from 3 or 4)

--- src/master-slave/src/fsm/fsm.go
+++ src/master-slave/src/fsm/fsm.go
@@ -33,25 +33,26 @@ const (
 var Elev_state int
 var Dir global.Motor_direction_t /// la til global variabel Dir (direction)
 var current_order queue.Order
+var Empty_list bool
 
 func State_handler(new_order_bool_chan chan bool, updated_order_bool_chan chan bool, update_order_chan chan queue.Order) {
 	fmt.Println("Running: State handler")
-	elev_state := Idle
+	Elev_state = Idle
 
 	for {
-		switch elev_state {
+		switch Elev_state {
 		case Idle:
 			event_idle(new_order_bool_chan)
 			Elev_state = Moving
-			elev_state = Moving
+			//elev_state = Moving
 		case Moving:
 			event_moving(update_order_chan)
-			Elev_state = Door_open
-			elev_state = Door_open
+			//Elev_state = Door_open
+			//elev_state = Door_open
 		case Door_open:
 			event_door_open(update_order_chan)
-			Elev_state = Idle
-			elev_state = Idle
+			//Elev_state = Idle
+			//elev_state = Idle
 			time.Sleep(1 * time.Second)
 		case Stuck:
 			os.Exit(0)
@@ -61,7 +62,7 @@ func State_handler(new_order_bool_chan chan bool, updated_order_bool_chan chan b
 }
 
 func event_idle(new_order_bool_chan chan bool) {
-	fmt.Println(" Now in Idle state")
+	fmt.Println("Running event: Idle.")
 	order_exist := false
 	fmt.Println("Current order is before", current_order)
 
@@ -71,6 +72,7 @@ func event_idle(new_order_bool_chan chan bool) {
 				current_order = queue.Internal_order_list[i]
 				fmt.Println("The current order is internal---------------------------------------")
 				order_exist = true
+				Empty_list = false
 				break
 			}
 		}
@@ -80,58 +82,63 @@ func event_idle(new_order_bool_chan chan bool) {
 		if queue.External_order_list[i].Order_state != queue.Inactive {
 			if queue.External_order_list[i].Order_state != queue.Finished {
 				if queue.External_order_list[i].Assigned_to == network.Local_ip || network.Num_elev_online == 0 {
-					fmt.Println("current order is external and belongs to ", network.Local_ip)
+					fmt.Println(queue.External_order_list[i].Assigned_to == network.Local_ip)
 					current_order = queue.External_order_list[i]
 					order_exist = true
+					Empty_list = false
 					break
 				}
 			}
 		}
 	}
 	fmt.Println("Current order is after", current_order)
-
+	Empty_list = true
 	if order_exist == false {
 		select {
 		case <-new_order_bool_chan:
+			this_order := current_order
 			new_order_Assigned_to_me := false
-			for {
-				//fmt.Println("Got new order bool ", catch_new_order_bool, " in Idle.")
-				//fmt.Println("Now checking for orders that needs to be done inside the select case in event_idle")
-				fmt.Println("Current order is", current_order)
-				for i := 0; i < global.NUM_INTERNAL_ORDERS; i++ {
-					if queue.Internal_order_list[i].Order_state != queue.Inactive {
-						if queue.Internal_order_list[i].Order_state != queue.Finished {
+			//fmt.Println("Got new order bool ", catch_new_order_bool, " in Idle.")
+			//fmt.Println("Now checking for orders that needs to be done inside the select case in event_idle")
+			fmt.Println("Current order is", current_order)
+			for i := 0; i < global.NUM_INTERNAL_ORDERS; i++ {
+				if queue.Internal_order_list[i].Order_state != queue.Inactive {
+					if queue.Internal_order_list[i].Order_state != queue.Finished {
 
-							fmt.Println("The current order is internal---------------------------------------")
-							current_order = queue.Internal_order_list[i]
-							new_order_Assigned_to_me = true
-							break
-						}
-					}
-					if new_order_Assigned_to_me == true {
+						fmt.Println("The current order is internal---------------------------------------")
+						//current_order = queue.Internal_order_list[i]
+						this_order = queue.Internal_order_list[i]
+						fmt.Println("This order is: ", this_order)
+						new_order_Assigned_to_me = true
+
 						break
 					}
-
 				}
-				for i := 0; i < global.NUM_GLOBAL_ORDERS; i++ {
-					if queue.External_order_list[i].Order_state != queue.Inactive {
-						if queue.External_order_list[i].Order_state != queue.Finished {
-							if queue.External_order_list[i].Assigned_to == network.Local_ip || network.Num_elev_online == 0 {
-								fmt.Println(queue.External_order_list[i].Assigned_to, "is equal to", network.Local_ip, "-----------------")
-								current_order = queue.External_order_list[i]
-								new_order_Assigned_to_me = true
-							}
+				if new_order_Assigned_to_me == true {
+					Empty_list = false
+					break
+				}
+
+			}
+			for i := 0; i < global.NUM_GLOBAL_ORDERS; i++ {
+				if queue.External_order_list[i].Order_state != queue.Inactive {
+					if queue.External_order_list[i].Order_state != queue.Finished {
+						if queue.External_order_list[i].Assigned_to == network.Local_ip || network.Num_elev_online == 0 {
+							fmt.Println(queue.External_order_list[i].Assigned_to, "is equal to", network.Local_ip, "-----------------", "network.Num_elev_onlibe", network.Num_elev_online)
+							current_order = queue.External_order_list[i]
+							new_order_Assigned_to_me = true
 						}
 					}
-
 				}
+
 				// PROBLEM : Tror 154 = 0.... ? whæt.
 				//Uten new_order_Assigned tome bool går den rett til door_open selv om den ikke faktisk har en ny ordre!
 				//Prøvd å fikse med for loop inni casen som breakes hvis new_order_Assigned_to_me
 				//Så finne ut hvorfor 0 == 154 er true......
 
 				if new_order_Assigned_to_me == true {
-					fmt.Println("trying to break")
+					fmt.Println("The order is assigned to me, I'll take it.")
+					Empty_list = false
 					break
 				}
 
@@ -142,8 +149,12 @@ func event_idle(new_order_bool_chan chan bool) {
 }
 
 func event_moving(update_order_chan chan queue.Order) {
+	fmt.Println("Running event: Moving.")
 	elevator_to_floor(current_order.Floor, update_order_chan)
-	// Elev_state = Door_open // <- sette global state inne i funksjonen
+	/*if Elev_state != Idle {
+		Elev_state = Door_open
+	}*/
+
 }
 
 func event_door_open(update_order_chan chan queue.Order) {
@@ -163,7 +174,7 @@ func event_door_open(update_order_chan chan queue.Order) {
 	go queue.Order_to_update_order_chan(current_order, update_order_chan)
 	fmt.Println("Order sent on updated order chan.")
 
-	//Elev_state = Idle // <- sette global state inne i funksjonen
+	Elev_state = Idle // <- sette global state inne i funksjonen
 }
 
 func elevator_to_floor(floor global.Floor_t, update_order_chan chan queue.Order) {
@@ -195,7 +206,7 @@ func elevator_to_floor(floor global.Floor_t, update_order_chan chan queue.Order)
 	current_floor_int := driver.Get_floor_sensor_signal()
 	current_floor := driver.Floor_int_to_floor_t(current_floor_int)
 	floor_int := driver.Floor_t_to_floor_int(floor)
-	fmt.Println(current_floor_int, floor_int, current_floor)
+	fmt.Println("Current floor int: ", current_floor_int, ", floor int: ", floor_int, ", current floor: ", current_floor)
 
 	if current_floor_int < floor_int {
 		fmt.Println("Going up.")
@@ -207,12 +218,16 @@ func elevator_to_floor(floor global.Floor_t, update_order_chan chan queue.Order)
 
 			// When arriving at any floor, check for order
 			if driver.Get_floor_sensor_signal() != -1 {
+				//fmt.Println("Floor sensor signal is not equal to minus one.")
 				this_floor := driver.Floor_int_to_floor_t(driver.Get_floor_sensor_signal())
 				driver.Set_floor_indicator_lamp(this_floor)
 				//pick_up_order_on_the_way(current_floor, order_list, updated_order_chan, current_order)
 				//time.Sleep(10 * time.Millisecond)
-				is_order := stop_if_order_in_floor(current_floor, update_order_chan)
+				//is_order := stop_if_order_in_floor(current_floor, update_order_chan)
+				is_order := check_if_order_in_floor(this_floor)
+				//fmt.Println("We have an order in this floor.")
 				if is_order {
+					Elev_state = Door_open
 					break
 				}
 				time.Sleep(10 * time.Millisecond)
@@ -237,8 +252,10 @@ func elevator_to_floor(floor global.Floor_t, update_order_chan chan queue.Order)
 				driver.Set_floor_indicator_lamp(this_floor)
 				//pick_up_order_on_the_way(current_floor, order_list, updated_order_chan, current_order)
 				//time.Sleep(10 * time.Millisecond)
-				is_order := stop_if_order_in_floor(current_floor, update_order_chan)
+				//is_order := stop_if_order_in_floor(current_floor, update_order_chan)
+				is_order := check_if_order_in_floor(this_floor)
 				if is_order {
+					Elev_state = Door_open
 					break
 				}
 				time.Sleep(10 * time.Millisecond)
@@ -248,6 +265,14 @@ func elevator_to_floor(floor global.Floor_t, update_order_chan chan queue.Order)
 				break
 			}
 		}
+	} else {
+		if current_order.Order_state == queue.Inactive || current_order.Order_state == queue.Finished {
+			Elev_state = Idle
+		} else {
+			Elev_state = Door_open
+			// was idle before, but it should open the door if it's in the same floor
+			// must delete the order somewhere, atm it's a loop
+		}
 	}
 
 	// Stop when at desired floor
@@ -255,19 +280,20 @@ func elevator_to_floor(floor global.Floor_t, update_order_chan chan queue.Order)
 	driver.Set_motor_direction(global.DIR_STOP)
 }
 
-func stop_if_order_in_floor(floor global.Floor_t, update_order_chan chan queue.Order) bool {
+/*func stop_if_order_in_floor(floor global.Floor_t, update_order_chan chan queue.Order) bool {
 	is_order_in_floor := check_if_order_in_floor(floor)
 	if is_order_in_floor {
 		driver.Set_motor_direction(global.DIR_STOP)
 		event_door_open(update_order_chan)
 	}
 	return is_order_in_floor
-}
+}*/
 
 func check_if_order_in_floor(floor global.Floor_t) bool {
 	for i := 0; i < global.NUM_INTERNAL_ORDERS; i++ {
 		if queue.Internal_order_list[i].Floor == floor && queue.Internal_order_list[i].Order_state != queue.Inactive {
 			current_order = queue.Internal_order_list[i]
+			//fmt.Println("The current order is: ", current_order, " , and the queue element is: ", queue.Internal_order_list[i])
 			return true
 		}
 	}
